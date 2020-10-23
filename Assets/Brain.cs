31200f5ff38704a175f05c451084fb6328d7bb53
JHoster <J.Hoster@web.de>
2020-10-23 16:10:30 +0200

    First version 0.1.0

---
+++ Assets/Brain.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brain : MonoBehaviour
+{
+    int DNALength = 2; //decision when seeing: nothing, ground //, death, obstacle
+    public float timeAlive;
+    public float distanceTravelled;
+    public int crash;
+    public DNA dna;
+    public GameObject eyes;
+    bool seeGround = true;
+    //bool seeObstacle = false;
+    //bool seeDeath = false;
+    bool alive = true;
+    Rigidbody2D rb;
+
+    public void Init()  //initialise DNA
+    {
+        dna = new DNA(DNALength, 3); //Number of reactions, here: 0 = moveForward, 1 = don'tJump, 2 = Jump
+        timeAlive = 0;
+        alive = true;
+        rb = this.GetComponent<Rigidbody2D>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "obstacle")
+        {
+            crash++;
+        }
+        if (collision.gameObject.tag == "death")
+        {
+            alive = false;
+        }
+    }
+
+    private void Update()
+    {
+        if (!alive) return;
+        seeGround = false;
+        //seeObstacle = false;
+        //seeDeath = false;
+        RaycastHit2D hit = Physics2D.Raycast(eyes.transform.position, eyes.transform.forward, 10.0f);
+
+        Debug.DrawRay(eyes.transform.position, eyes.transform.forward * 10.0f, Color.red);
+
+        if (hit.collider != null)
+        {
+            //if (hit.collider.gameObject.tag == "obstacle")
+            //    seeObstacle = true;
+            //if (hit.collider.gameObject.tag == "death")
+            //    seeDeath = true;
+            if (hit.collider.gameObject.tag == "ground")
+                seeGround = true;
+        }
+        else
+            seeGround = false; //also seeObstacle = false //also seeDeath = false;
+        timeAlive = PopulationManager.elapsed;
+
+        float upforce = 0;
+        float forwardForce = 0;
+
+        //if (seeObstacle)
+        //    upforce = dna.GetGene(0);
+        //if (seeDeath)
+        //    upforce = dna.GetGene(1);
+        if (seeGround)
+        {
+            if (dna.GetGene(0) == 0) forwardForce = 10;
+            else if (dna.GetGene(0) == 1) upforce = 0;
+            else if (dna.GetGene(0) == 2) upforce = 1;
+        }
+        else
+        {
+            if (dna.GetGene(1) == 0) forwardForce = 10;
+            else if (dna.GetGene(1) == 1) upforce = 0;
+            else if (dna.GetGene(1) == 2) upforce = 1;
+        }
+
+        rb.AddForce(this.transform.right * forwardForce);
+        //rb.AddForce(this.transform.up * upforce);
+        rb.AddForce(this.transform.up * upforce, ForceMode2D.Impulse); //Jumping needs groundin!
+
+        distanceTravelled = Vector2.Distance(this.transform.position, PopulationManager.startPos.position);
+    }
+}
---
+++ Assets/PopulationManager.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class PopulationManager : MonoBehaviour
+{
+    public GameObject botPrefab;
+    public int populationSize = 50;
+    List<GameObject> population = new List<GameObject>();
+    public static float elapsed = 0;
+    public float trialTime = 5;
+    int generation = 1;
+    public static Transform startPos;
+
+    GUIStyle guiStyle = new GUIStyle();
+    private void OnGUI()
+    {
+        guiStyle.fontSize = 25;
+        guiStyle.normal.textColor = Color.white;
+        GUI.BeginGroup(new Rect(10, 10, 250, 150));
+        GUI.Box(new Rect(0, 0, 140, 140), "Stats", guiStyle);
+        GUI.Box(new Rect(10, 25, 200, 30), "Gen: " + generation, guiStyle);
+        GUI.Box(new Rect(10, 50, 200, 30), string.Format("Time: {0:0.00}", elapsed), guiStyle);
+        GUI.Box(new Rect(10, 75, 200, 30), "Population: " + population.Count, guiStyle);
+        GUI.EndGroup();
+    }
+
+    private void Start()
+    {
+        startPos = transform;
+        for (int i = 0; i < populationSize; i++)
+        {
+            GameObject b = Instantiate(botPrefab, startPos.position, startPos.rotation);
+            b.GetComponent<Brain>().Init();
+            population.Add(b);
+        }
+    }
+
+    GameObject Breed(GameObject parent1, GameObject parent2)
+    {
+        GameObject offspring = Instantiate(botPrefab, startPos.position, startPos.rotation);
+        Brain b = offspring.GetComponent<Brain>();
+        if (Random.Range(0, 100) == 1) // mutate 1 in 100
+        {
+            b.Init();
+            b.dna.Mutate();
+        }
+        else
+        {
+            b.Init();
+            b.dna.Combine(parent1.GetComponent<Brain>().dna, parent2.GetComponent<Brain>().dna);
+        }
+        return offspring;
+
+    }
+
+    void BreedNewPopulation() //Fitness 
+    {
+        //List<GameObject> sortedList = population.OrderBy(o => o.GetComponent<Brain>().timeAlive).ToList(); //Time
+        List<GameObject> sortedList = population.OrderBy(o => o.GetComponent<Brain>().distanceTravelled).ToList(); //Distance
+        //Crahes?
+
+        population.Clear();
+        //breed upper half of sorted list
+        for (int i = (int)(sortedList.Count / 2.0f) - 1; i < sortedList.Count - 1; i++)
+        {
+            population.Add(Breed(sortedList[i], sortedList[i + 1]));
+            population.Add(Breed(sortedList[i + 1], sortedList[i]));
+        }
+        //destroy all parents and previous population
+        for (int i = 0; i < sortedList.Count; i++)
+        {
+            Destroy(sortedList[i]);
+        }
+        generation++;
+    }
+
+    private void Update()
+    {
+        elapsed += Time.deltaTime;
+        if (elapsed >= trialTime)
+        {
+            BreedNewPopulation();
+            elapsed = 0;
+        }
+    }
+}
