6a911b11539cf994a8f42e53bdc15fdc10d4ac83
JHoster <J.Hoster@web.de>
2020-10-30 16:47:28 +0100

    Added Player. Version 0.1.2

---
+++ Assets/Player.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    bool alive = true;
+    bool grounded = true;
+    Rigidbody2D rb;
+    Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+        anim = this.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (grounded)
+            {
+                grounded = false;
+                rb.AddForce(this.transform.up * 100, ForceMode2D.Impulse);
+            }
+        }
+        if(Input.GetKey(KeyCode.D))
+            rb.AddForce(this.transform.right * 100);
+
+        //Anim
+        if (rb.velocity.y > 0)
+        {
+            anim.SetBool("jumping", true);
+            anim.SetBool("running", false);
+            anim.SetBool("falling", false);
+        }
+        else if (rb.velocity.y < 0)
+        {
+            anim.SetBool("falling", true);
+            anim.SetBool("running", false);
+            anim.SetBool("jumping", false);
+        }
+        else if (rb.velocity.x != 0)
+        {
+            anim.SetBool("running", true);
+            anim.SetBool("falling", false);
+            anim.SetBool("jumping", false);
+        }
+        else
+        {
+            anim.SetBool("running", false);
+            anim.SetBool("falling", false);
+            anim.SetBool("jumping", false);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "death")
+        {
+            //New iteration
+        }
+        if (collision.gameObject.tag == "ground")
+        {
+            grounded = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+}
