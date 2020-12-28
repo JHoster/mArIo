74092ad074601a24e312280258488b180a40a190
Johannes Hoster <J.Hoster@web.de>
2020-12-28 14:56:50 +0100

    Added UnitTests. Version 0.2.2

---
+++ Assets/UnitTest/Tests/TestSuite.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class TestSuite
+{
+    private GameObject game;
+    private GameObject obs;
+    private GameObject bot;
+
+    [UnityTest]
+    public IEnumerator GamePausesUntilPlay()
+    {
+        game = MonoBehaviour.Instantiate(Resources.Load<GameObject>("Game"));
+        Assert.True(Menu.GameIsPaused);
+        game.transform.Find("Canvas").GetComponent<Menu>().Resume();
+        Assert.False(Menu.GameIsPaused);
+        yield return null;
+    }
+
+    [UnityTest]
+    public IEnumerator ObstacleMovesUpAndDown()
+    {
+        obs = MonoBehaviour.Instantiate(Resources.Load<GameObject>("Obs"));
+        float initialYPos = obs.transform.position.y;
+        yield return new WaitForSeconds(0.1f);
+        Assert.Greater(obs.transform.position.y, initialYPos);
+        yield return new WaitForSeconds(0.9f);
+        Assert.Less(obs.transform.position.y, initialYPos);
+        GameObject.Destroy(obs);
+    }
+
+    [UnityTest]
+    public IEnumerator OnObstacleCollisionBotDies()
+    {
+        bot = GameObject.Find("Bot(Clone)");
+        obs = MonoBehaviour.Instantiate(Resources.Load<GameObject>("Obs"));
+        bot.transform.position = obs.transform.position;
+        yield return new WaitForSeconds(1f);
+        Assert.Greater(bot.GetComponent<Brain>().crash, Player.crashMax);
+    }
+}
